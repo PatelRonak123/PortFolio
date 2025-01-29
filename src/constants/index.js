7857f6dc6dc94d66a112772403d4ec142f5949d5
Ronak Patel <email.otherworks@gmail.com>
2025-01-30 01:52:19 +0530

    last commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -221,7 +221,7 @@ const projects = [
       },
     ],
     image: portfolio,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/PatelRonak123/PortFolio",
   },
   {
     name: "UBER",
